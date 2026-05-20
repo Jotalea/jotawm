494e7c4f6c587f08f757f27abf55e893edfd97b7
Jotalea <main@jotalea.com.ar>
2026-05-19 23:08:41 -0300

    feat: create pictures directory on /home/jotalea if it doesn't exist"
    invasive? perhaps. useful? yes.

--- jwm-session.c
+++ jwm-session.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 int main(void)
 {
+    const char *home = getenv("HOME");
+    if (home) {
+        char path[4096];
+        snprintf(path, sizeof(path), "%s/Pictures", home);
+        mkdir(path, 0755);
+    }
     setenv("QT_QPA_PLATFORMTHEME",      "qt6ct", 1);
     setenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1",   1);
     setenv("GDK_BACKEND",               "x11",   1);
